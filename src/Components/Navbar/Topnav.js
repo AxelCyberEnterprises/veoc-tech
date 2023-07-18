269541ef3ad16b6f9b081b5f3812a46ac487d4a3
unknown <ibrahimtaiwoti@gmail.com>
2023-07-18 03:18:57 +0100

    some changes to the home navbar

--- src/Components/Navbar/Topnav.js
+++ src/Components/Navbar/Topnav.js
@@ -13,29 +13,32 @@ function Topnav(props) {
   } 
 
   return (
-    <div className={props.blue? "topnav__container-blue": "topnav__container" }>
-      <div className="topnav__container-left">
-        <img src={VTS} alt="logo.png" />
-      </div>
-      <div className="topnav__container-right">
-        <div className={props.blue? 'topnav__container-links-blue' : 'topnav__container-links'}>
-          <NavLink exact to="/" className="custom-link">Home</NavLink>
-          <NavLink to="/About" className="custom-link">About us</NavLink>
-          <NavLink to="/Corporate" className="custom-link">Corporate</NavLink>
-          <NavLink to="/Services" className="custom-link">Services</NavLink>
-          <NavLink to="/Portfolio" className="custom-link">Portfolio</NavLink>
-          <NavLink to="/" className="custom-link">Contact</NavLink>
+    <>
+      <div className={props.blue? "topnav__container-blue": "topnav__container" }>
+        <div className="topnav__container-left">
+          <img src={VTS} alt="logo.png" />
         </div>
-        <div className='topnav__container-button'>
-          <button>Get a quote</button>
+        <div className="topnav__container-right">
+          <div className={props.blue? 'topnav__container-links-blue' : 'topnav__container-links'}>
+            <NavLink exact to="/" className="custom-link">Home</NavLink>
+            <NavLink to="/About" className="custom-link">About us</NavLink>
+            <NavLink to="/Corporate" className="custom-link">Corporate</NavLink>
+            <NavLink to="/Services" className="custom-link">Services</NavLink>
+            <NavLink to="/Portfolio" className="custom-link">Portfolio</NavLink>
+            <NavLink to="/" className="custom-link">Contact</NavLink>
+          </div>
+          <div className='topnav__container-button'>
+            <button>Get a quote</button>
+          </div>
         </div>
-      </div>
+
+      </div>  
       <div className="topnav__container-ham">
         <div onClick={handleClick} className="topnav__container-hambuger"> 
           <img src={hamburger} alt="hambuger-img"/>
         </div>
         <div className="topnav__container-hambuger_links">
-         {trigger && (
+        {trigger && (
             <ul className="topnav__container-hambuger_links-container">
               <li><Link className="ham-link" to="/">Home</Link></li>
               <li><Link className="ham-link" to="/About">About us</Link></li>
@@ -44,12 +47,10 @@ function Topnav(props) {
               <li><Link className="ham-link" to="/Portfolio">Portfolio</Link></li>
               <li><Link className="ham-link" to="/">Contact</Link></li>
           </ul>
-         ) } 
+        ) } 
         </div>
       </div>
-
-    </div>  
-    
+    </>
   )
 }
 
